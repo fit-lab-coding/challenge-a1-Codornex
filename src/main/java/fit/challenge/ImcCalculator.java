c76e0651aae44fe080954c42e86dc4ddb9c6aacf
Deivid <davis_43102@hotmail.com>
2021-02-03 12:42:08 -0300

    .

--- src/main/java/fit/challenge/ImcCalculator.java
+++ src/main/java/fit/challenge/ImcCalculator.java
@@ -2,7 +2,7 @@ package fit.challenge;
 
 public class ImcCalculator {
     public static String calculate(double vPeso, double vAltura) {
-        double imc = vPeso / (vAltura * vAltura);
+        double imc = vPeso / (Math.pow(vAltura, 2));
         if (imc < 18.5) {
             return "Magreza";
         } else if (imc < 24.9) {
